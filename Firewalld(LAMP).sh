1d57f705b4cb1faf71ada2bf1c96afd37c9eb45c
MitDan98 <56722622+MitDan98@users.noreply.github.com>
2019-10-28 11:43:10 +0200

    Update Firewalld(LAMP).sh

--- Firewalld(LAMP).sh
+++ Firewalld(LAMP).sh
@@ -4,5 +4,30 @@
 #Firewall command for httpd 
 #LAMP hint 1
 sudo firewall-cmd --permanent --zone=public --add-service=http
+if($? -ne 0); then
+ echo "Was added with succes"
+ exit 1
+
+elif 
+echo"Error!"
+exit 1
+fi
+ 
 sudo firewall-cmd --permanent --zone=public --add-service=https
+if($? -ne 0); then
+ echo "Was added with succes"
+ exit 1
+
+elif 
+echo"Error!"
+exit 1
+fi
 sudo firewall-cmd --zone=public --permanent --add-port=80
+if($? -ne 0); then
+ echo "Was added port with succes"
+ exit 1
+
+elif 
+echo"Error!"
+exit 1
+fi
